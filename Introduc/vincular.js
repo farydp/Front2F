499edc4b8bfee1f77080b0ddace80ab714dee989
farydp <farydalejandro997@hotmail.com>
2024-02-24 19:57:03 -0500

    Primer commit para front 2

---
+++ Introduc/vincular.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Nombre");
+alert(" Su nombre es " + nombre);
+
+let pregunta= confirm( nombre + ", Eri gay?");
+if(pregunta){
+    alert("aaaaaah bueeeeeee")
+}
+else{
+    alert("Parabéns Master")
+}
+
+console.log(Math.max(23,24,26,1));
